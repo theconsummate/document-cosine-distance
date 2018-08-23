94461ee8ec4668237fc940e3fc201f115e1c50c6
Dhruv Mishra <ubiquitiousdm@gmail.com>
2018-08-23 18:29:42 +0200

    using google custom search api and adding italian embeddings

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ pip3 install -r requirements.txt
 ```
 
 ### Download glove embeddings
+#### English
 Url: https://nlp.stanford.edu/projects/glove/
 Url for smallest 6 billion corpus: http://nlp.stanford.edu/data/glove.6B.zip
 
@@ -15,9 +16,31 @@ Extract the zip file and copy the glove.6B.50d.txt file into the data folder.
 python3 -m gensim.scripts.glove2word2vec --input data/glove.6B.50d.txt --output data/word2vec.glove.6B.50d.txt
 ```
 
+#### Italian
+Download from http://hlt.isti.cnr.it/wordembeddings/glove_wiki_window10_size300_iteration50.tar.gz and then copy the extracted files into data folder
+
+### Create GoogleAPI keys
+#### Create an API key
+Go to https://developers.google.com/custom-search/json-api/v1/overview and 'Get a Key' button in the API key section. Follow the steps along.
+
+#### Custom Search engine key
+Go to https://cse.google.com/cse/ and create a key. In the 'sites to search field', add
+
+```
+*.com
+*.co.uk
+*.it
+```
+
+Next, create a file keys.py in this directory and put your keys here
+```
+developer_key=<Developer_key>
+search_engine_key=<Custom Search engine key>
+```
+
 ### Compute the cosine-distance
 ```
-python3 main "<query string>"
+python3 main <lang_code> "<query string>"
 ```
 
 Retrieves the top 10 search results for the input query string from Google and computes the distance of the first result with the other 9 results.
--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 # -*- coding: utf-8 -*-
 from gensim.models.keyedvectors import KeyedVectors
+from gensim.models import Word2Vec
 from DocSim import DocSim
-from lib.google_search_results import GoogleSearchResults
+from googleapiclient.discovery import build
 import pprint
 import sys
+import keys
 
-model_path = './data/word2vec.glove.6B.50d.txt'
-stopwords_path = "./data/stopwords_en.txt"
+model_path_en = './data/word2vec.glove.6B.50d.txt'
+model_path_it = './data/glove_WIKI'
+stopwords_path_en = "./data/stopwords_en.txt"
+stopwords_path_it = "./data/stopwords-it.txt"
 pp = pprint.PrettyPrinter()
 
-def query_word2vec(source_doc, target_docs):
-    model = KeyedVectors.load_word2vec_format(model_path, binary=False)
-    with open(stopwords_path, 'r') as fh:
-        stopwords = fh.read().split(",")
+def load_model(language):
+    if language == "en":
+        model = KeyedVectors.load_word2vec_format(model_path_en, binary=False)
+        with open(stopwords_path_en, 'r') as fh:
+            stopwords = fh.read().split(",")
+        return model, stopwords
+
+    elif language == "it":
+        model = Word2Vec.load(model_path_it)
+        with open(stopwords_path_it, 'r') as fh:
+            stopwords = [x.strip() for x in fh.readlines()]
+        return model, stopwords
+    else:
+        print("Invalid language code. Only en and it supported")
+        sys.exit()
+
+def query_word2vec(source_doc, target_docs, language):
+    model, stopwords = load_model(language)
     ds = DocSim(model,stopwords=stopwords)
 
     sim_scores = ds.calculate_similarity(source_doc, target_docs)
@@ -20,34 +38,51 @@ def query_word2vec(source_doc, target_docs):
     print(sim_scores)
 
 
-def get_search_results(query_str):
-    params = {
-        "q" : query_str,
-        # "location" : "Austin, Texas, United States",
-        "hl" : "en",
-        "google_domain" : "google.com",
-        "api_key" : "demo",
-        "safe" : "active",
-        "num" : "10",
-    }
-
-    query = GoogleSearchResults(params)
-    dictionary_results = query.get_dictionary()['organic_results']
+def get_search_results(query_str, language="en"):
+    if language == "en":
+        domain = "google.com"
+        lr = "lang_en"
+    elif language == "it":
+        domain = "google.it"
+        lr = "lang_it"
+    # params = {
+    #     "q" : query_str,
+    #     # "location" : "Austin, Texas, United States",
+    #     "hl" : language,
+    #     "google_domain" : domain,
+    #     "api_key" : "demo",
+    #     "safe" : "active",
+    #     "num" : "10",
+    # }
+    service = build("customsearch", "v1", developerKey=keys.developer_key)
+    query = service.cse().list(
+                               q=query_str,
+                               hl=language,
+                               lr=lr,
+                               googlehost=domain,
+                               cx=keys.search_engine_key,
+                               # cx='017576662512468239146:omuauf_lfve',
+                               ).execute()
+    print(query)
+    dictionary_results = query['items']
 
     docs = []
-    for result in dictionary_results:
-        doc = {"id": result["position"], "content": result["snippet"], "link": result["link"], "title": result["title"]}
+    for i in range(len(dictionary_results)):
+        result = dictionary_results[i]
+        doc = {"id": i, "content": result["snippet"], "link": result["link"], "title": result["title"]}
         docs.append(doc)
     return docs
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("""Please provide an input query string to get search results. Run like this:
-python3 main.py "<query string>" """)
+python3 main.py <language_code> "<query string>"
+possible values for language_code: en or it""")
         sys.exit()
-    query_str = sys.argv[1]
-    docs = get_search_results(query_str)
+    language = sys.argv[1]
+    query_str = sys.argv[2]
+    docs = get_search_results(query_str, language)
     pp.pprint(docs)
     print("computing similarity ...")
-    query_word2vec(docs[0]["content"], docs[1:])
+    query_word2vec(docs[0]["content"], docs[1:], language)
 
